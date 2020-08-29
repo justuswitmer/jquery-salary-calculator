6602b8b696a9bbab8394e2fb44a46e0484933008
Justus Witmer <justus.witmer@gmail.com>
2020-08-29 09:40:26 -0500

    Showing info in table and summing salary

--- client.js
+++ client.js
@@ -13,22 +13,44 @@ let employeeInfo = [
 
 function addEmployeeInfo() {
     console.log('in addEmployeeInfo');
-    // get employee info and place into a new object
-    const newEmployee = {
-        firstName: $('#firstNameIn').val(),
-        lastName: $('#lastNameIn').val(),
-        employeeId: $('#employeeIdIn').val(),
-        title: $('#jobTitleIn').val(),
-        annualSalary: $('#annualSalaryIn').val()
-    } // end newEmployee
-    // push newEmployee into employeeInfo
-    employeeInfo.push(newEmployee);
-    console.log(employeeInfo);
-    displayEmployee();
+    let firstName = $('#firstNameIn').val();
+    let lastName = $('#lastNameIn').val();
+    let employeeId = $('#employeeIdIn').val();
+    let title = $('#jobTitleIn').val();
+    let annualSalary = $('#annualSalaryIn').val();
+    // if statement to check that all fields are filled
+    if (!firstName || !lastName || !employeeId || !title || !annualSalary) {
+        alert('All fields must be filled.')
+    } // end if
+    else {
+        // get employee info and place into a new object
+        const newEmployee = {
+            firstName: $('#firstNameIn').val(),
+            lastName: $('#lastNameIn').val(),
+            employeeId: $('#employeeIdIn').val(),
+            title: $('#jobTitleIn').val(),
+            annualSalary: $('#annualSalaryIn').val()
+        } // end newEmployee
+        // push newEmployee into employeeInfo
+        employeeInfo.push(newEmployee);
+        // remove text from fields
+        $('#firstNameIn').val('');
+        $('#lastNameIn').val('');
+        $('#employeeIdIn').val('');
+        $('#jobTitleIn').val('');
+        $('#annualSalaryIn').val('');
+        console.log(employeeInfo);
+        displayEmployee();
+    } // end else
 } // end addEmployeeInfo
 
 function calcMonthlyCost() {
     // loop through employeeInfo
+    let monthlyCosts = 0;
+    for (i = 0; i < employeeInfo.length; i++) {
+        monthlyCosts += Number(employeeInfo[i].annualSalary)
+    } // end for
+    console.log(monthlyCosts);
     // sum employeeInfo.annualSalary
     // append sum of employeeInfo.annualSalary to DOM
     // if monthly costs > $20,000, add red background color to monthly costs
@@ -37,15 +59,20 @@ function calcMonthlyCost() {
 function displayEmployee() {
     console.log('in displayEmployee');
     let el = $('#employeeInfoOut');
-    el.empty();
     // append employeeInfo to DOM, clear fields
+    el.append(`
+       <tr><th>First Name</th><th>Last Name</th><th>Employee ID</th>
+      <th>Job Title</th><th>Annual Salary</th></tr>
+    `);
+
     for (let i = 0; i < employeeInfo.length; i++) {
         el.append(`
-            <li>${employeeInfo[i].firstName} ${employeeInfo[i].lastName}
-            (${employeeInfo[i].employeeId}) – ${employeeInfo[i].title}:
-             $${employeeInfo[i].annualSalary}</li>
+            <tr><td>${employeeInfo[i].firstName}</td><td>${employeeInfo[i].lastName}</td>
+            <td>${employeeInfo[i].employeeId}</td><td>${employeeInfo[i].title}</td>
+             <td>$${employeeInfo[i].annualSalary}</td></tr>
         `);
-    } // end for loop  
+    } // end for loop
+    calcMonthlyCost();
 } // end displayEmployee
 
 function onReady() {
